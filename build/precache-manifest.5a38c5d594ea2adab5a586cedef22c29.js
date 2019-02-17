8b48e7202a0016caf7d0814ac07acfa0c8f38cc9
KEV-PC\Kevin <kngovand@gmail.com>
2019-02-16 20:17:12 -0700

    deploying...

---
+++ build/precache-manifest.5a38c5d594ea2adab5a586cedef22c29.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e32ddfc7d3789e82a7ae9ed15876bcaf",
+    "url": "/static/media/snow2.e32ddfc7.jpeg"
+  },
+  {
+    "revision": "49ca00a12385f55be7fe",
+    "url": "/static/css/main.ce830a2b.chunk.css"
+  },
+  {
+    "revision": "e39a6e8c9a29344c11360753a933eb0d",
+    "url": "/static/media/codes.e39a6e8c.jpeg"
+  },
+  {
+    "revision": "b25d88c91f67811d75cc",
+    "url": "/static/js/1.b25d88c9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49ca00a12385f55be7fe",
+    "url": "/static/js/main.49ca00a1.chunk.js"
+  },
+  {
+    "revision": "581cc66751512df2233c75b96b41fbc1",
+    "url": "/static/media/keyboard.581cc667.jpg"
+  },
+  {
+    "revision": "88abab5e0b22439eeb26e31cecee6ffe",
+    "url": "/static/media/pexels.88abab5e.jpeg"
+  },
+  {
+    "revision": "3ce4ab63d4e4702943a36875ff99c28e",
+    "url": "/static/media/blue.3ce4ab63.jpg"
+  },
+  {
+    "revision": "b25d88c91f67811d75cc",
+    "url": "/static/css/1.53ecad66.chunk.css"
+  },
+  {
+    "revision": "ae74fbc5f6d4b8e4bcf38368d1ce54ae",
+    "url": "/index.html"
+  }
+];
